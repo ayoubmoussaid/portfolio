bff2a6393112b67ea7f8b656124ffbf2d3d87f68
rain man <45740145+ayoubmoussaid@users.noreply.github.com>
2020-10-10 10:34:03 +0100

    added jsx purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  purge: ["./components/**/*.tsx", "./pages/**/*.tsx", "./components/**/*.jsx"],
   theme: {
     extend: {
       animation: {
