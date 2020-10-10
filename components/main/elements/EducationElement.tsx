bfae505845f92e50aed8377546a7d26f90a7d0f8
rain man <45740145+ayoubmoussaid@users.noreply.github.com>
2020-10-10 11:06:55 +0100

    fixed education layout

--- components/main/elements/EducationElement.tsx
+++ components/main/elements/EducationElement.tsx
@@ -4,7 +4,7 @@ import { Education } from '../../../interfaces'
 
 const EducationElement: FC<Education> = ({data}) => {
     return (
-        <div className='flex flex-col sm:flex-row w-full border-t border-b border-gray-400 sm:justify-between sm:px-5 sm:py-4 py-2 sm:items-center items-start'>
+        <div className='flex flex-col sm:flex-row w-full border-t border-b border-gray-400 justify-between sm:px-5 sm:py-4 py-2 items-center '>
             <div className="sm:p-1 py-2 w-8 h-8 rounded-full border border-gray-300 text-gray-600 flex justify-center items-center">{data.id}</div>
             <div className="sm:p-1 py-2 sm:w-1/4 text-xs font-bold text-gray-800">{data.date}</div>
             <div className="sm:p-1 py-2 sm:w-1/4 text-sm text-gray-800 font-semibold">{data.location}</div>
