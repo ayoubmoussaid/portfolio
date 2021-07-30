bae5e3123934339030f8737f583ad077c1f6e22a
rain man <45740145+ayoubmoussaid@users.noreply.github.com>
2021-07-30 15:53:03 +0100

    Updated work experience

--- data/experience.ts
+++ data/experience.ts
@@ -2,18 +2,25 @@
 const experience = [
     {
         id: 1,
+        company: 'Oracle Labs North Africa',
+        description: 'Contribution to developement and benchmark of Oracle Property Graph technology (Java, PGQL, PGX)',
+        task: 'Internship-Research Assistant',
+        date: 'February-August 2021'
+    },
+    {
+        id: 2,
         company: 'Ministry of National Education, UM6P and 1337',
         description: 'Development of the Front End of TelmidTice-mobile app for educational videos streaming-using the Flutter Framework for cross platform app development',
         task: 'Flutter Dev',
         date: 'JULY & AUGUST 2020'
     },
     {
-        id: 2,
+        id: 3,
         company: 'Caisse de depot et de gestion - RABAT',
         description: 'Development of a Desktop Java application for handling monetary transactions. Front-end done using JavaFX, PhpMyAdmin for MySQL database and Java for everything else.',
         task: 'Internship-Java Dev',
         date: 'JULY 2019'
-    },
+    }
 ]
 
 export default experience
