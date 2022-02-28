68a3fdad9ef401e4e65b97132e2a5fc969d80a73
rain man <45740145+ayoubmoussaid@users.noreply.github.com>
2022-02-28 21:08:14 +0100

    Update experience.ts

--- data/experience.ts
+++ data/experience.ts
@@ -3,7 +3,7 @@ const experience = [
     {
         id: 1,
         company: 'Oracle',
-        description: 'Software engineer, working on graph technologies (PGQL)',
+        description: 'Software engineer, working on Oracle graph technologies (PGQL)',
         task: 'Member of Techinical Staff',
         date: 'OCTOBER 2022-PRESENT'
     },
